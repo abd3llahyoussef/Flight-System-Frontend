6156a0c6a623d87bbd9c3c7173f01ff06d31abc1
Abdallah Youssef <88988496+abd3llahyoussef@users.noreply.github.com>
2026-05-09 03:17:05 +0300

    modify for cloud

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,45 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+export default defineConfig(({ mode }) => {
+  // تحميل متغيرات البيئة بناءً على 'mode' (development أو production)
+  const env = loadEnv(mode, process.cwd(), '');
+
+  // تحديد التارغت بناءً على البيئة
+  const apiTarget = mode === 'production'
+    ? 'http://13.50.13.206:8080'
+    : 'http://localhost:8080';
+
+  return {
+    server: {
+      host: "::",
+      port: 5173,
+      proxy: {
+        "/api": {
+          target: apiTarget,
+          changeOrigin: true,
+        },
+      },
+      hmr: {
+        overlay: false,
+      },
+    },
+    plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+      dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
+    },
+  };
+});
+
+
+
+
+/*import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
@@ -25,5 +66,5 @@ export default defineConfig(({ mode }) => ({
     },
     dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
   },
-}));
+}));*/
 
